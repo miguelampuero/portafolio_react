666628bf1c99377dd2f38c5dc53e5de00e2ed9c5
Miguel Ampuero <mampueroh2022@gmail.com>
2025-05-21 01:45:24 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+
+function LocationLogger() {
+  const location = useLocation();
+  console.log("Ruta actual:", location.pathname);
+  return null;
+}
 
 function App() {
   return (
     <>
       <Toaster />
       <HashRouter>
+        <LocationLogger />
         <Routes>
-          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
