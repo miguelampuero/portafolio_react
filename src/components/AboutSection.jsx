661673ba4ae85c12aec97efa47a773cd172c4fbd
Dekszx <147951736+alexishh25@users.noreply.github.com>
2025-12-27 11:08:29 -0500

    actualizacion de cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a> */}
 
               <a
-                href="https://drive.google.com/file/d/1if58uOF7R7xN25I9FsbLbfNWBhI3oj1e/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1yn83mhc0CDkTzlHZOn7khOkm-GCsXWNG/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Descargar CV
